8b19c97f6836bb48ee84d6c2fe7edcaf4e116d1e
maryam <111368504+maryammaryam9@users.noreply.github.com>
2025-12-20 22:22:22 +0500

    12-20-2025

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -10,18 +10,21 @@ export class CategoriesComponent {
     {
       imageSrc: '../../../assets/domestic.jpeg',
       title: 'Domestic Electrician',
-      description: 'The SCV Electrical domestic electrician team provide a wide range of electrical services ranging from lighting installations and rewiring to smart home integrations.'
+      description:
+        'The SCV Electrical domestic electrician team provide a wide range of electrical services ranging from lighting installations and rewiring to smart home integrations.'
     },
     {
       imageSrc: '../../../assets/EICR.jpeg',
-      title: 'EICR',
-      description: 'SCV Electrical provides a comprehensive and efficient EICR service that ensures compliance, safety, and reliability for our clients.'
-    }
-    ,
+      title: 'EICR, MWC, EIC',
+      description:
+        'SCV Electrical provides a comprehensive and efficient electrical certification service ensuring compliance, safety, and reliability for our clients.'
+    },
     {
       imageSrc: '../../../assets/Commercial.png',
       title: 'Commercial Electrician',
-      description: 'Our comprehensive commercial electrical offering is designed to address the complex electrical needs of modern businesses, empowering our clients with reliability, safety, and energy efficiency.'
+      description:
+        'Our comprehensive commercial electrical offering is designed to address the complex electrical needs of modern businesses, empowering our clients with reliability, safety, and energy efficiency.'
     }
   ];
+
 }
